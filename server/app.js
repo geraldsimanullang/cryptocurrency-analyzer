de3fa9c121e21a0237ac12c35966298a224bf7e4
geraldsimanullang <geraldsimanullang@gmail.com>
2024-09-06 04:08:52 +0700

    feat: env setting

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const cors = require("cors");
 const express = require("express");
 const app = express();
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 
 app.use(
   cors({
